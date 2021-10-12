456c0cae2a95d9bb24ab1c07952b72ef68868009
Sean Maas <seanmaas27@gmail.com>
2021-10-12 19:20:58 -0400

    Implement ALU opcodes

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ BUILD_DIR := build
 SRC_DIRS  := src
 
 SFILES := $(foreach dir,$(SRC_DIRS),$(wildcard $(dir)/*.S))
+HFILES := $(foreach dir,$(SRC_DIRS),$(wildcard $(dir)/*.h))
 OFILES := $(patsubst %.S,$(BUILD_DIR)/%.o,$(SFILES))
 
 include $(N64_INST)/include/n64.mk
@@ -12,6 +13,8 @@ all: $(PROJ_NAME).z64
 $(PROJ_NAME).z64: N64_ROM_TITLE="$(PROJ_NAME)"
 
 $(BUILD_DIR)/$(PROJ_NAME).elf: $(OFILES)
+
+$(OFILES): $(HFILES)
 
 clean:
 	rm -rf $(BUILD_DIR) $(PROJ_NAME).z64
---
+++ src/cpu_address.h
@@ -0,0 +1,206 @@
+#include <regdef.h>
+
+.macro IM8
+    // Get the address of the 8-bit immediate value
+    add  a0, s7, 1
+    addi s7, s7, 2
+.endm
+
+.macro I16
+    // Get the address of the 16-bit immediate value
+    add  a0, s7, 1
+    addi s7, s7, 3
+.endm
+
+.macro DIR
+    // Get the 8-bit immediate value added to the direct offset as an address
+    addi a0, s7, 1
+    jal  memory_read8
+    lhu  a0, direct_ofs
+    add  a0, a0, v0
+    addi s7, s7, 2
+.endm
+
+.macro DRX
+    // Get the 8-bit immediate value plus register X added to the direct offset as an address
+    addi a0, s7, 1
+    jal  memory_read8
+    lhu  t0, register_x
+    add  t0, t0, v0
+    andi t0, t0, 0xFF
+    lhu  a0, direct_ofs
+    add  a0, a0, t0
+    addi s7, s7, 2
+.endm
+
+.macro DRY
+    // Get the 8-bit immediate value plus register Y added to the direct offset as an address
+    addi a0, s7, 1
+    jal  memory_read8
+    lhu  t0, register_y
+    add  t0, t0, v0
+    andi t0, t0, 0xFF
+    lhu  a0, direct_ofs
+    add  a0, a0, t0
+    addi s7, s7, 2
+.endm
+
+.macro DRS
+    // Get the 8-bit immediate value added to the stack pointer as an address
+    addi a0, s7, 1
+    jal  memory_read8
+    lhu  a0, stack_ptr
+    add  a0, a0, v0
+    addi s7, s7, 2
+.endm
+
+.macro ABS
+    // Get the 16-bit immediate value added to the data bank as an address
+    addi a0, s7, 1
+    jal  memory_read16
+    lbu  a0, data_bank
+    sll  a0, a0, 16
+    add  a0, a0, v0
+    addi s7, s7, 3
+.endm
+
+.macro ABX
+    // Get the 16-bit immediate value plus register X added to the data bank as an address
+    addi a0, s7, 1
+    jal  memory_read16
+    lhu  t0, register_x
+    add  t0, t0, v0
+    lbu  a0, data_bank
+    sll  a0, a0, 16
+    add  a0, a0, t0
+    addi s7, s7, 3
+.endm
+
+.macro ABY
+    // Get the 16-bit immediate value plus register Y added to the data bank as an address
+    addi a0, s7, 1
+    jal  memory_read16
+    lhu  t0, register_y
+    add  t0, t0, v0
+    lbu  a0, data_bank
+    sll  a0, a0, 16
+    add  a0, a0, t0
+    addi s7, s7, 3
+.endm
+
+.macro LNG
+    // Get the 24-bit immediate value as an address
+    addi a0, s7, 1
+    jal  memory_read16
+    move s0, v0
+    addi a0, s7, 3
+    jal  memory_read8
+    sll  a0, v0, 16
+    add  a0, a0, s0
+    addi s7, s7, 4
+.endm
+
+.macro LNX
+    // Get the 24-bit immediate value plus register X as an address
+    addi a0, s7, 1
+    jal  memory_read16
+    lhu  s0, register_x
+    add  s0, s0, v0
+    addi a0, s7, 3
+    jal  memory_read8
+    sll  a0, v0, 16
+    add  a0, a0, s0
+    addi s7, s7, 4
+.endm
+
+.macro IND
+    // Get the 8-bit immediate value added to the direct offset as an address
+    addi a0, s7, 1
+    jal  memory_read8
+    lhu  a0, direct_ofs
+    add  a0, a0, v0
+
+    // Read a 16-bit value from memory as an address
+    jal  memory_read16
+    move a0, v0
+    addi s7, s7, 2
+.endm
+
+.macro IDX
+    // Get the 8-bit immediate value plus register X added to the direct offset as an address
+    addi a0, s7, 1
+    jal  memory_read8
+    lhu  t0, register_x
+    add  t0, t0, v0
+    andi t0, t0, 0xFF
+    lhu  a0, direct_ofs
+    add  a0, a0, t0
+
+    // Read a 16-bit value from memory as an address
+    jal  memory_read16
+    move a0, v0
+    addi s7, s7, 2
+.endm
+
+.macro IDY
+    // Get the 8-bit immediate value added to the direct offset as an address
+    addi a0, s7, 1
+    jal  memory_read8
+    lhu  a0, direct_ofs
+    add  a0, a0, v0
+
+    // Read a 16-bit value from memory plus register Y as an address
+    jal  memory_read16
+    lhu  a0, register_y
+    add  a0, a0, v0
+    addi s7, s7, 2
+.endm
+
+.macro ISY
+    // Get the 8-bit immediate value added to the stack pointer as an address
+    addi a0, s7, 1
+    jal  memory_read8
+    lhu  a0, stack_ptr
+    add  a0, a0, v0
+
+    // Read a 16-bit value from memory plus register Y as an address
+    jal  memory_read16
+    lhu  a0, register_y
+    add  a0, a0, v0
+    addi s7, s7, 2
+.endm
+
+.macro IDL
+    // Get the 8-bit immediate value added to the direct offset as an address
+    addi a0, s7, 1
+    jal  memory_read8
+    lhu  a0, direct_ofs
+    add  a0, a0, v0
+
+    // Read a 24-bit value from memory as an address
+    jal  memory_read16
+    move s0, v0
+    addi a0, a0, 2
+    jal  memory_read8
+    sll  a0, v0, 16
+    add  a0, a0, s0
+    addi s7, s7, 2
+.endm
+
+.macro ILY
+    // Get the 8-bit immediate value added to the direct offset as an address
+    addi a0, s7, 1
+    jal  memory_read8
+    lhu  a0, direct_ofs
+    add  a0, a0, v0
+
+    // Read a 24-bit value from memory plus register Y as an address
+    jal  memory_read16
+    lhu  s0, register_y
+    add  s0, s0, v0
+    addi a0, a0, 2
+    jal  memory_read8
+    sll  a0, v0, 16
+    add  a0, a0, s0
+    addi s7, s7, 2
+.endm
